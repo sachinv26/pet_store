ae5cf9f66aff884aeb6613d701a9fe3171c2040b
sachinv26 <vermas7008@gmail.com>
2022-01-04 20:26:09 +0530

    secondpagedone

---
+++ lib/landingpage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+class MySecondPage extends StatefulWidget {
+  @override
+  _MySecondPageState createState() => _MySecondPageState();
+}
+class _MySecondPageState extends State<MySecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go back to Home Screen'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:petstore/landingpage.dart';
 
 void main() {
   runApp(MaterialApp(
